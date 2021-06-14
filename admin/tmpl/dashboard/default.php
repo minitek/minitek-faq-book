802910bdfb9714f7e41e8c1782bce31a7c78864c
Yannis Maragos <ioannismaragos@gmail.com>
2021-06-14 19:06:46 +0300

    Small changes in dashboard

--- admin/tmpl/dashboard/default.php
+++ admin/tmpl/dashboard/default.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
-* @title		Minitek FAQ Book
-* @copyright	Copyright (C) 2011-2021 Minitek, All rights reserved.
-* @license		GNU General Public License version 3 or later.
-* @author url	https://www.minitek.gr/
-* @developers	Minitek.gr
-*/
+ * @title		Minitek FAQ Book
+ * @copyright	Copyright (C) 2011-2021 Minitek, All rights reserved.
+ * @license		GNU General Public License version 3 or later.
+ * @author url	https://www.minitek.gr/
+ * @developers	Minitek.gr
+ */
 
 defined('_JEXEC') or die;
 
@@ -23,12 +24,12 @@ $localVersion = FAQBookProHelper::localVersion();
 		<div class="col-sm-12 col-md-8">
 			<div class="media mt-0">
 				<div class="float-start">
-					<img class="media-object" src="<?php echo URI::root(true).'/media/com_faqbookpro/css/logo.png'; ?>">
+					<img class="media-object" src="<?php echo URI::root(true) . '/media/com_faqbookpro/css/logo.png'; ?>">
 				</div>
 				<div class="media-body">
-			    <h2 class="media-heading"><?php echo Text::_('COM_FAQBOOKPRO'); ?> <span class="badge bg-success">Free</span></h2>
-			    <?php echo Text::_('COM_FAQBOOKPRO_DESC'); ?>
-			  </div>
+					<h2 class="media-heading"><?php echo Text::_('COM_FAQBOOKPRO'); ?> <span class="badge bg-success">Free</span></h2>
+					<?php echo Text::_('COM_FAQBOOKPRO_DESC'); ?>
+				</div>
 			</div>
 
 			<div class="dashboard-thumbnails">
@@ -110,7 +111,7 @@ $localVersion = FAQBookProHelper::localVersion();
 				</div>
 
 				<div class="thumbnail card">
-					<a href="<?php echo Route::_('index.php?option=com_config&view=component&component=com_faqbookpro&path=&return='.base64_encode(URI::getInstance()->toString())); ?>">
+					<a href="<?php echo Route::_('index.php?option=com_config&view=component&component=com_faqbookpro&path=&return=' . base64_encode(URI::getInstance()->toString())); ?>">
 						<i class="icon icon-cog"></i>
 						<span class="thumbnail-title">
 							<?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_CONFIGURATION'); ?>
@@ -175,37 +176,37 @@ $localVersion = FAQBookProHelper::localVersion();
 					<div class="card-body p-0">
 						<ul class="list-group list-group-flush">
 							<li class="list-group-item">
-								<span class="icon-book" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-book icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/support/documentation/joomla/minitek-faq-book" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_SIDEBAR_DOCUMENTATION'); ?></a>
 								</span>
 							</li>
 							<li class="list-group-item">
-								<span class="icon-list" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-list icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/support/changelogs/joomla/minitek-faq-book" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_SIDEBAR_CHANGELOG'); ?></a>
 								</span>
 							</li>
 							<li class="list-group-item">
-								<span class="icon-support" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-support icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/support/forum/joomla/minitek-faq-book" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_SIDEBAR_TECHNICAL_SUPPORT'); ?></a>
 								</span>
 							</li>
 							<li class="list-group-item">
-								<span class="icon-help" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-help icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/support/documentation/joomla/minitek-faq-book/faq" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_SIDEBAR_FAQ'); ?></a>
 								</span>
 							</li>
 							<li class="list-group-item">
-								<span class="icon-question" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-question icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/support/documentation/joomla/minitek-faq-book/free-vs-pro" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_SIDEBAR_FREE_VS_PRO'); ?></a>
 								</span>
 							</li>
 							<li class="list-group-item">
-								<span class="icon-lock" aria-hidden="true"></span>&nbsp;
+								<span class="me-2 icon-lock icon-fw" aria-hidden="true"></span>&nbsp;
 								<span>
 									<a href="https://www.minitek.gr/joomla/extensions/minitek-faq-book#subscriptionPlans" target="_blank"><?php echo Text::_('COM_FAQBOOKPRO_DASHBOARD_UPGRADE_TO_PRO'); ?></a>
 								</span>
