ac68807804414059de3a413d74eaed454e1a6b15
Minitek <info@minitek.gr>
2022-07-26 14:52:31 +0300

    Replaced method getTopicsTree with getTopicsSlider

--- site/src/Helper/NavigationHelper.php
+++ site/src/Helper/NavigationHelper.php
@@ -25,11 +25,11 @@ use Joomla\Registry\Registry;
  */
 abstract class NavigationHelper
 {
-	public static function getTopicsTree($item, $show_icons, $level = 1)
+	public static function getTopicsSlider($item, $show_icons, $level = 1)
 	{
 		$topicModel = new TopicModel;
 		$topicParams = new Registry($item->params);
-  		$output = '';
+  		$html = '';
 		$subitems = $topicModel->getTopicChildren($item->id);
 
 		if (count($subitems))
@@ -47,28 +47,28 @@ abstract class NavigationHelper
 			}
 			else
 			{
-		  		$item_class = '';
+		  		$item_class = 'NavLeftUL_parent';
 				$icon = '<span class="NavLeftUL_navIcon fas fa-chevron-right"></span>';
 			}
 
-			$output .= '<li id="liid'.$item->id.'" class="NavLeftUL_item NavLeftUL_topic '.$item_class.'">';
-			$output .= '<a href="'.Route::_(RouteHelper::getTopicRoute($item->id, false, false, $item->language)).'" class="NavLeftUL_anchor" onclick="return false;">';
-			$output .= '<span class="topicTitle">';
+			$html .= '<li id="liid'.$item->id.'" class="NavLeftUL_item NavLeftUL_topic '.$item_class.'">';
+			$html .= '<a href="'.Route::_(RouteHelper::getTopicRoute($item->id, false, false, $item->language)).'" class="NavLeftUL_anchor" onclick="return false;">';
+			$html .= '<span class="topicTitle">';
 
 			if ($show_icons && $topicParams->get('topic_icon_class', ''))
 			{
-				$output .= '<i class="NavLeftUL_topicIcon '.$topicParams->get('topic_icon_class', '').'"></i>';
+				$html .= '<i class="NavLeftUL_topicIcon '.$topicParams->get('topic_icon_class', '').'"></i>';
 			}
 
-			$output .= $item->title;
-			$output .= '</span>';
-			$output .= $icon;
-			$output .= '</a>';
-			$output .= '<ul class="NavLeftUL_sublist level'.$level.'">';
+			$html .= $item->title;
+			$html .= '</span>';
+			$html .= $icon;
+			$html .= '</a>';
+			$html .= '<ul class="NavLeftUL_sublist level'.$level.'">';
 
 			foreach ($subitems as $subitem)
 			{
-				$output .= self::getTopicsTree($subitem, $show_icons, $level + 1);
+		  		$html .= self::getTopicsSlider($subitem, $show_icons, $level + 1);
 			}
 
 			$back_href = $item->parent_id > 1 
@@ -88,22 +88,22 @@ abstract class NavigationHelper
 				$data_title = $section->title;
 			}
 			
-			$output .= '<li data-parent="'.$item->parent_id.'" class="NavLeftUL_backItem">';
-			$output .= '<a href="'.$back_href.'" class="NavLeftUL_anchor" rel="nofollow" onclick="return false;" data-title="'.$data_title.'">';
-			$output .= '<span>'.Text::_('COM_FAQBOOKPRO_BACK').'<span class="NavLeftUL_navBackIcon fas fa-arrow-left"></span></span>';
-			$output .= '</a></li>';
-			$output .= '</ul>';
-			$output .= '</li>';
+			$html .= '<li data-parent="'.$item->parent_id.'" class="NavLeftUL_backItem">';
+			$html .= '<a href="'.$back_href.'" class="NavLeftUL_anchor" rel="nofollow" onclick="return false;" data-title="'.$data_title.'">';
+			$html .= '<span>'.Text::_('COM_FAQBOOKPRO_BACK').'<span class="NavLeftUL_navBackIcon fas fa-arrow-left"></span></span>';
+			$html .= '</a></li>';
+			$html .= '</ul>';
+			$html .= '</li>';
 		}
 		else
 		{
-	  		$output .= '<li id="liid'.$item->id.'" class="NavLeftUL_item NavLeftUL_topic NavLeftUL_endpoint">';
-			$output .= '<a href="'.Route::_(RouteHelper::getTopicRoute($item->id, false, false, $item->language)).'" class="NavLeftUL_anchor" onclick="return false;">';
-			$output .= '<span class="topicTitle">';
+	  		$html .= '<li id="liid'.$item->id.'" class="NavLeftUL_item NavLeftUL_topic NavLeftUL_endpoint">';
+			$html .= '<a href="'.Route::_(RouteHelper::getTopicRoute($item->id, false, false, $item->language)).'" class="NavLeftUL_anchor" onclick="return false;">';
+			$html .= '<span class="topicTitle">';
 
 			if ($show_icons && $topicParams->get('topic_icon_class', ''))
 			{
-				$output .= '<i class="NavLeftUL_topicIcon '.$topicParams->get('topic_icon_class', '').'"></i>';
+				$html .= '<i class="NavLeftUL_topicIcon '.$topicParams->get('topic_icon_class', '').'"></i>';
 			}
 
 			$icon = '<div class="NavLeftUL_endpointIcon">'.
@@ -112,14 +112,14 @@ abstract class NavigationHelper
 				'</div>'.
 				'</div>';
 
-			$output .= $item->title;
-			$output .= '</span>';
-			$output .= $icon;
-			$output .= '</a>';
-			$output .= '</li>';
+			$html .= $item->title;
+			$html .= '</span>';
+			$html .= $icon;
+			$html .= '</a>';
+			$html .= '</li>';
 		}
 
-		return $output;
+		return $html;
 	}
 
 	public static function getTopicDepth($item, $levels)
